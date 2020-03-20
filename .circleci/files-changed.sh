cd94711543febb88bbeca1ae173db2b1438bd584
Adrian Price-Whelan <adrian.prw@gmail.com>
2020-03-20 16:45:54 -0400

    boop

--- .circleci/files-changed.sh
+++ .circleci/files-changed.sh
@@ -1,17 +1,9 @@
 #!/bin/bash -eux
 
 if [[ -z $CIRCLE_PULL_REQUEST ]] ; then
-    git checkout --orphan rst
-    git add -f tutorials/rst-tutorials/*
-    git -c user.name='circle' -c user.email='circle' commit -m "now with RST"
-
-    branches="$(git remote -v)"
-    if [[ $branches != *"origin"* ]]; then
-        git remote add origin git@github.com:astropy/astropy-tutorials.git
-    fi
-    git push -q -f origin rst
-    echo "Not a pull request: pushing RST files to rst branch."
+    echo "Not a pull request"
 else
     echo $CIRCLE_PULL_REQUEST
-    echo "This is a pull request: not pushing RST files."
+    FILES_CHANGED="$(git --no-pager diff --name-only FETCH_HEAD $(git merge-base FETCH_HEAD origin/master))"
+    echo $FILES_CHANGED
 fi
